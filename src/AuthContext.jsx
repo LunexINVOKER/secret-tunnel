2904de50aa8686e79a656d8cb03f20e9e70cf5cb
LunexINVOKER <karapuz2209@gmail.com>
2026-03-13 11:35:56 -0400

    completed

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -8,11 +8,29 @@ export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
 
-  // TODO: signup
+  async function signup(name) {
+    const res = await fetch(API + "/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: name }),
+    });
+    const json = await res.json();
+    if (!res.ok) throw new Error(json.message);
+    setToken(json.token);
+    setLocation("TABLET");
+  }
 
-  // TODO: authenticate
+  async function authenticate() {
+    if (!token) throw new Error("No token found.");
+    const res = await fetch(API + "/authenticate", {
+      headers: { Authorization: "Bearer " + token },
+    });
+    const json = await res.json();
+    if (!res.ok) throw new Error(json.message);
+    setLocation("TUNNEL");
+  }
 
-  const value = { location };
+  const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
